cbf3588b140cd06f556aeb24d4563a934c62697b
Dayane <dayaneknlang@gmail.com>
2021-08-23 17:53:53 -0300

    Ongoing project (created libraries, structured route, API CRUD ok)

--- nutchache-challenge/src/App.js
+++ nutchache-challenge/src/App.js
@@ -1,22 +1,12 @@
-import logo from './assets/nutcache-logo.png';
-import './App.css';
+import React from 'react';
+import Routes from './routes/routes';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo-nutcache" />
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
